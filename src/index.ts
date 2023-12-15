d99da0eba8243ca466e7138baf5adb86de77917c
Devraj <khanradevraj@gmail.com>
2023-12-15 11:40:24 +0530

    setup update

--- src/index.ts
+++ src/index.ts
@@ -6,33 +6,37 @@ import folderGeneratorModule from "./modules/folder";
 import fs from "fs";
 import path from "path";
 
-// Get the user's operating system and home directory
-const userEnvironment = environment.detectUserEnvironment();
+function initialSetup() {
+  // Get the user's operating system and home directory
+  const userEnvironment = environment.detectUserEnvironment();
 
-if (userEnvironment) {
-  const [userOS, homeDirectory] = userEnvironment;
-  const data: boolean = fs.existsSync(path.join(homeDirectory, "data"));
-  // Save user configuration to a JSON file
-  if (!data) {
-    const userConfig: UserConfig = {
-      operatingSystem: userOS,
-      homeDirectory,
-      folder: {
-        data: true,
-        subfolders: {
-          stock: false,
-          indice: false,
-          nifty: false,
+  if (userEnvironment) {
+    const [userOS, homeDirectory] = userEnvironment;
+    const data: boolean = fs.existsSync(path.join(homeDirectory, "data"));
+    // Save user configuration to a JSON file
+    if (!data) {
+      const userConfig: UserConfig = {
+        operatingSystem: userOS,
+        homeDirectory,
+        folder: {
+          data: true,
+          subfolders: {
+            stock: false,
+            indice: false,
+            nifty: false,
+          },
         },
-      },
-    };
+      };
 
-    userConfigModule.saveUserConfig(userConfig);
+      userConfigModule.saveUserConfig(userConfig);
 
-    // Generate folders based on the user configuration
-    folderGeneratorModule.generateFolders(userConfig);
+      // Generate folders based on the user configuration
+      folderGeneratorModule.generateFolders(userConfig);
+    }
+  } else {
+    // Handle the case when the operating system is unsupported
+    console.error("Exiting application due to unsupported operating system.");
   }
-} else {
-  // Handle the case when the operating system is unsupported
-  console.error("Exiting application due to unsupported operating system.");
 }
+
+export default initialSetup
